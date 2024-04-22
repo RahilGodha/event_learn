bb21459a51f3294f89c99ccef2e35ac86cdc8104
upasanatharu <worship9771@gmail.com>
2024-04-22 16:09:51 +0530

    First

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    publicRoutes: [
+        '/',
+        'events/:id',
+        'api/webhook/clerk',
+        'api/webhook/stripe',
+        'api/uploadthing'
+    ],
+    ignoredRoutes: [
+        '/assets/images/logo.svg',
+        'api/webhook/clerk',
+        'api/webhook/stripe',
+        'api/uploadthing'
+    ]
+});
+
+export const config = {
+    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
